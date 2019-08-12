f46a2c16c3dbc608c8d4a27c41471f804f140edc
hongch <hongch@ufotosoft.com>
2019-08-12 17:00:18 +0800

    t

--- addons/hong_hui/wxapp.php
+++ addons/hong_hui/wxapp.php
@@ -1,12 +1,21 @@
 <?php
-
-
-class wxapp
-{
-    function doPageImage()
-    {
+/**
+ * 小程序入口
+ * [WeEngine System] Copyright (c) 2013 WE7.CC
+ */
+defined('IN_IA') or exit('Access Denied');
+class Hong_huiModuleWxapp extends WeModuleWxapp {
+    //public $token = 'we7_testhooktoken'; //接口通信token
+    public function doPageIndex() { // 接口一个名为"index"的接口
+        $this->result(0, '', array('test' => 1235)); //  响应json串
+    }
+    public function doPageTest() {
+    }
+    public function doPageImage(){
         load()->func('logging');
         logging_run('doPageImage');
-        return json_encode(['a'=>'c']);
+
+        $this->result(0, '', array('test' => 1235)); //  响应json串
+
     }
 }
