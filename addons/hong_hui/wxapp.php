d91d5ff7f1ec932fe8239f7188e6b235ab2e3ef3
hongch <hongch@ufotosoft.com>
2019-08-13 15:19:10 +0800

    t

--- addons/hong_hui/wxapp.php
+++ addons/hong_hui/wxapp.php
@@ -47,7 +47,7 @@ class Hong_huiModuleWxapp extends WeModuleWxapp {
     public function doPageVideo(){
         global  $_GPC,$_W;
         load()->func('logging');
-        logging_run('doPageYunimage:'.json_encode($_GPC));
+        logging_run('doPageVideo:'.json_encode($_GPC));
         $url=$_GPC['url'];
         $session_id=$_W['session_id'];//round(microtime(true) * 1000);
         exec('php /home/wwwroot/default/redbook/yii redbook start '.$session_id.' '.$url.' 2',$array);
@@ -59,8 +59,7 @@ class Hong_huiModuleWxapp extends WeModuleWxapp {
             fclose($myfile);
         }
         logging_run('txt:'.json_encode($txt));
-        $this->result(0, '', array('lunimg'=>'https://ci.xiaohongshu.com/daeb8bca-40d6-5dee-8fe9-54e8dec0a9d6?imageView2/2/w/1080/format/jpg',
-            'video'=>'https://v.xiaohongshu.com/23237a577db9f8c168c49652da23b68b06656cfa?sign=35424ab0553a467a449508a013dc69d7&t=5d52de80','size'=>'9')); //  响应json串
+        $this->result(0, '', json_decode($txt)); //  响应json串
     }
     public function doPageYunvideo(){
         $this->result(0, '', array('lunimg'=>'https://ci.xiaohongshu.com/daeb8bca-40d6-5dee-8fe9-54e8dec0a9d6?imageView2/2/w/1080/format/jpg',
