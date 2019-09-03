d69493f912a3fa403eafdc1c9cdac2a38ce1e0b0
hongch <hongch@ufotosoft.com>
2019-09-03 16:40:56 +0800

    t

--- addons/hong_duanshipin/wxapp.php
+++ addons/hong_duanshipin/wxapp.php
@@ -60,7 +60,8 @@ class Hong_duanshipinModuleWxapp extends WeModuleWxapp {
             $this->result(1, '视频解析失败,请检查视频地址是否正确！', []);
         }else{
             $video=$link_data['data']['video'];
-            $path='/home/wwwroot/default/videos/';
+            $path='/home/wwwroot/default/we7/attachment/videos/';
+            logging_run('doPageQuery——$video:'.$video);
             $this->downFile($video,$path);
             $this->result(1, '视频解析失败,请检查视频地址是否正确！', ['downurl'=>'www']);
 
